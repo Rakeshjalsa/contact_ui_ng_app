0b406c38ffd86488b988edfe7cc88dcad28ce285
Rakeshjalsa <155987542+Rakeshjalsa@users.noreply.github.com>
2024-02-25 14:23:27 +0530

    Update contact.service.ts

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://65.0.168.78:8081/"
+  private baseUrl="a4b1ef634259a408fa32b2a4ec9d5463-739675695.ap-south-1.elb.amazonaws.com"
 
   constructor(private httpClient:HttpClient) { }
 
